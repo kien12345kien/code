b5e7b8604a0b48af35f36e64fa7becbce28626a1
kien12345kien <48080802+kien12345kien@users.noreply.github.com>
2024-07-18 14:24:03 +0700

    Update binary.cpp

--- cpp/DSA/tree/binary.cpp
+++ cpp/DSA/tree/binary.cpp
@@ -14,5 +14,24 @@ TreeNode* init(TreeNode* tree, int val){
     tree = new TreeNode;
     tree->val = val;
     tree->tLeft = nullptr;
-    tree->tRight = nullptr;    
+    tree->tRight = nullptr;
+    return tree;    
+}
+
+void NLR(TreeNode* tree){
+    if(tree == nullptr) return;
+
+    cout << tree->val;
+
+    NLR(tree->tLeft);
+    NLR(tree->tRight);
+}
+
+TreeNode* insert(TreeNode* tree, int x){
+    if(tree == nullptr){
+        tree = init(tree,x);
+        return tree;
+    }
+
+    if (x == tree->val);
 }
