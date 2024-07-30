0ce0490d9bc2ef998241980a19c1b6d32fe34559
kien12345kien <48080802+kien12345kien@users.noreply.github.com>
2024-07-30 23:09:37 +0700

    INSTALL VEVN
    install pwn python
    update You either know, XOR you don't.py

--- CRY(venv)/You either know, XOR you don't.py
+++ CRY(venv)/You either know, XOR you don't.py
@@ -1,17 +1,5 @@
-encrypt_msg = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
-encrypt_msg = bytes.fromhex(encrypt_msg)
-
-flag_format = b"crypto{"
-
-key = [o1 ^ o2 for (o1, o2) in zip (encrypt_msg, flag_format)] + [ord("y")]
-
-flag = []
-key_len = len(key)
-for i in range(len(encrypt_msg)):
-    flag.append(
-        encrypt_msg[i] ^ key[i % key_len]
-    )
-flag = "".join(chr(o) for o in flag)
-
-print("Flag: ")
-print(flag)
+from pwn import xor
+flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
+print(xor(flag, 'crypto{'.encode())) # oh, it says 'myXORke+y...'
+print('crypto{'.encode())
+print(xor(flag, 'myXORkey'.encode())) # try this? yay, it works! sometimes simpler is better
