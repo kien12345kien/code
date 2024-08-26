5d60eea2a8410c25e87746b3c075916da22d594d
kien12345kien <48080802+kien12345kien@users.noreply.github.com>
2024-08-26 17:50:54 +0700

    testing fstream

---
+++ cpp/FILE/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	ofstream fout;
+
+	string line;
+
+	fout.open("sample.txt");
+
+	while (fout){
+		getline(cin, line);
+
+		
+	}
+}
